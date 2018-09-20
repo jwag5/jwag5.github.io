1e3a9a689dfec24d1af6905c81904c5f1d773478
Jake Wagner <33435070+jwag5@users.noreply.github.com>
2018-09-20 12:26:19 -0700

    Create mechanics.js

---
+++ mechanics.js
@@ -0,0 +1,17 @@
+function gotoAbout(){
+  document.querySelector('#About').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+}
+
+function gotoPortfolio(){
+  document.querySelector('#Portfolio').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+}
+
+function gotoEducation(){
+  document.querySelector('#Education').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+}
